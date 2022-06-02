e60f2a071e6baebf5fa2f89080a191feee307241
Bretl7 <bret.landen97@gmail.com>
2022-06-01 19:26:14 -0700

    Add project files.

---
+++ CustomAuthStateProvider.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace BlazorApp1
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCIsImN0eSI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYm9iIiwiZXhwIjoxNjU0MjE5NDAxfQ.cjUsgJMLa_30jcKSYkaPFN-y9AZ9HJdFzOQePL5Ut2i2FwRyycZsetQ6fN9hce998LyY1UVJqQBm54-_lNLFZA";
+            var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+            //var identity = new ClaimsIdentity();
+           var user = new ClaimsPrincipal(identity);
+
+            var state = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+
+        }
+
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }   
+}
